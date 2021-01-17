2694956233a1d104aba8dc17502c045141cffc09
Elena Moskvina <lenka___93@mail.ru>
2021-01-17 22:32:31 +0300

    add hw5 *

--- src/dnd.js
+++ src/dnd.js
@@ -27,6 +27,19 @@ const homeworkContainer = document.querySelector('#homework-container');
    homeworkContainer.appendChild(newDiv);
  */
 function createDiv() {
+  let divsize = ((Math.random() * 100) + 50).toFixed();
+  let color = '#'+ Math.round(0xffffff * Math.random()).toString(16);
+  var posx = (Math.random() * document.documentElement.clientWidth - divsize).toFixed();
+  var posy = (Math.random() * document.documentElement.clientWidth - divsize).toFixed();
+  let newDiv = document.createElement('div');
+  newDiv.classList.add('draggable-div');
+  newDiv.style.cssText = `width: ${divsize}px;
+      height: ${divsize}px;
+      background-color: ${color};
+      position: absolute;
+      left: ${posx}px;
+      top: ${posy}px`;
+  return newDiv;
 }
 
 /*
@@ -38,6 +51,24 @@ function createDiv() {
    addListeners(newDiv);
  */
 function addListeners(target) {
+  document.querySelector('body').addEventListener('mousedown', (event) => {
+    const { target, clientX, clientY, pageX, pageY } = event;
+    const shiftX = clientX - target.getBoundingClientRect().left;
+    const shiftY = clientY - target.getBoundingClientRect().top;
+    moveAt(pageX, pageY);
+
+    function moveAt({ pageX, pageY }) {
+        target.style.left = pageX - shiftX + 'px';
+        target.style.top = pageY - shiftY + 'px';
+    }
+
+    document.addEventListener('mousemove', moveAt);
+    target.onmouseup = () => {
+      document.removeEventListener('mousemove', moveAt);
+      target.onmouseup = null;
+    };
+  });
+  document.body.ondragstart = () => false;
 }
 
 let addDivButton = homeworkContainer.querySelector('#addDiv');
